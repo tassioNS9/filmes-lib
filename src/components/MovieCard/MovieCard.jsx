bbabc1bd54b4160bb39b71613843e58b262a6436
Tassio Neves <tassions@hotmail.com>
2022-09-23 10:11:40 -0300

    update in div

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 import './styles.css'
 
 const imageURL = import.meta.env.VITE_IMG;
 
-
+//`/movie/${movie.id}
 const MovieCard = ({movie, showLink = true}) =>{
+
+    const navigate = useNavigate();
     return(
-        <div className="movie-card">
+        <div className="movie-card" onClick={()=>navigate(`/movie/${movie.id}`)}>
             <img src={imageURL + movie.poster_path} alt={movie.title}/>
             <h2>{movie.title}</h2>
             <p>
